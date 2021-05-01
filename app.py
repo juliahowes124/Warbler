05d0d8e998056de0e272626c8ab52246f5d29bc5
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-05-01 14:53:06 -0700

    remove block button if own profile, let admin override blocking

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def check_if_blocked(func):
     @wraps(func)
     def wrap(*args, **kwargs):
         user = User.query.get(kwargs.get("user_id"))
-        if g.user.is_blocked(user):
+        if g.user.is_blocked(user) and not g.user.is_admin:
             flash("This user has blocked you.", "warning")
             return redirect("/users")
         return func(*args, **kwargs)
